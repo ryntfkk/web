d1dbf770c11a108a13bbeea3c787606a96e14aee
Ryan taufik <86566731+ryntfkk@users.noreply.github.com>
2026-07-04 03:57:13 +0700

    new

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.poskojasa.com/api/v1';
 
 export async function fetchAPI<T>(
   endpoint: string,
