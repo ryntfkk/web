f2b57a38e977d3bcecb3cfc1eee48ede90f8be80
Ryan taufik <86566731+ryntfkk@users.noreply.github.com>
2026-07-11 18:41:28 +0700

    new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  // @ts-ignore
+  allowedDevOrigins: ['192.168.0.127', 'localhost'],
   async headers() {
     return [
       {
